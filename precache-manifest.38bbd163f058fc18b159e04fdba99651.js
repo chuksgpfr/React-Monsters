a8e705e3303b0d527533dd492c6482825ba95fab
Chukwudi Ubah <chuksgpfr@gmail.com>
2020-04-06 18:34:20 +0100

    Updates

---
+++ precache-manifest.38bbd163f058fc18b159e04fdba99651.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138ce47e3155499f654c3912fd496d4f",
+    "url": "/React-Monsters/index.html"
+  },
+  {
+    "revision": "e4584cf31096841e344d",
+    "url": "/React-Monsters/static/css/main.5cccddc0.chunk.css"
+  },
+  {
+    "revision": "a58e1ba730d0c1b0b6be",
+    "url": "/React-Monsters/static/js/2.9ace2ea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Monsters/static/js/2.9ace2ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4584cf31096841e344d",
+    "url": "/React-Monsters/static/js/main.5129d51b.chunk.js"
+  },
+  {
+    "revision": "c6fd6a47da7ff13cab23",
+    "url": "/React-Monsters/static/js/runtime-main.6edb7973.js"
+  }
+]);
